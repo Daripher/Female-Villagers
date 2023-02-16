72a15a8f364d3c4bba7dbef7a622331dce3ca39b
Daripher <saelver.serbek@gmail.com>
2023-02-16 10:27:22 +0500

    Replaced female illager renderers with one generic class

--- src/main/java/daripher/femalevillagers/client/render/FemaleIllagerRenderer.java
+++ src/main/java/daripher/femalevillagers/client/render/FemaleIllagerRenderer.java
@@ -2,24 +2,25 @@ package daripher.femalevillagers.client.render;
 
 import daripher.femalevillagers.FemaleVillagersMod;
 import daripher.femalevillagers.client.model.FemaleIllagerModel;
-import daripher.femalevillagers.entity.FemaleIllusioner;
 import net.minecraft.client.model.IllagerModel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.IllagerRenderer;
 import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.monster.AbstractIllager;
 
-public class FemaleIllusionerRenderer extends IllagerRenderer<FemaleIllusioner> {
-	private static final ResourceLocation SKIN_LOCATION = new ResourceLocation(FemaleVillagersMod.MOD_ID, "textures/entity/female_illusioner.png");
+public class FemaleIllagerRenderer<T extends AbstractIllager> extends IllagerRenderer<T> {
+	private final ResourceLocation skinLocation;
 
-	public FemaleIllusionerRenderer(EntityRendererProvider.Context context) {
+	public FemaleIllagerRenderer(EntityRendererProvider.Context context, String texture) {
 		super(context, new IllagerModel<>(context.bakeLayer(FemaleIllagerModel.LAYER_LOCATION)), 0.5F);
-		addLayer(new ItemInHandLayer<>(this));
+		skinLocation = new ResourceLocation(FemaleVillagersMod.MOD_ID, "textures/entity/" + texture + ".png");
+		addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()));
 		model.getHat().visible = true;
 	}
 
 	@Override
-	public ResourceLocation getTextureLocation(FemaleIllusioner villager) {
-		return SKIN_LOCATION;
+	public ResourceLocation getTextureLocation(T illager) {
+		return skinLocation;
 	}
 }
