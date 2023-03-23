b9c71077e4316352b2f070a427e5715497209ca4
Daripher <saelver.serbek@gmail.com>
2023-03-23 21:21:28 +0500

    Special class for item in hand render layer

--- src/main/java/daripher/femalevillagers/client/render/FemaleVindicatorRenderer.java
+++ src/main/java/daripher/femalevillagers/client/render/FemaleVindicatorRenderer.java
@@ -1,30 +1,23 @@
 package daripher.femalevillagers.client.render;
 
-import com.mojang.blaze3d.vertex.PoseStack;
+import java.util.function.Predicate;
 
 import daripher.femalevillagers.FemaleVillagersMod;
 import daripher.femalevillagers.client.model.FemaleIllagerModel;
+import daripher.femalevillagers.client.render.layer.HideableItemInHandLayer;
 import daripher.femalevillagers.entity.FemaleVindicator;
 import net.minecraft.client.model.IllagerModel;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.IllagerRenderer;
-import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.resources.ResourceLocation;
 
 public class FemaleVindicatorRenderer extends IllagerRenderer<FemaleVindicator> {
 	private static final ResourceLocation SKIN_LOCATION = new ResourceLocation(FemaleVillagersMod.MOD_ID, "textures/entity/female_vindicator.png");
+	private static final Predicate<FemaleVindicator> SHOULD_SHOW_ITEM_IN_HAND = e -> e.isAggressive();
 
 	public FemaleVindicatorRenderer(EntityRendererProvider.Context context) {
 		super(context, new IllagerModel<>(context.bakeLayer(FemaleIllagerModel.LAYER_LOCATION)), 0.5F);
-		addLayer(new ItemInHandLayer<FemaleVindicator, IllagerModel<FemaleVindicator>>(this, context.getItemInHandRenderer()) {
-			public void render(PoseStack p_116352_, MultiBufferSource p_116353_, int p_116354_, FemaleVindicator entity, float p_116356_, float p_116357_,
-					float p_116358_, float p_116359_, float p_116360_, float p_116361_) {
-				if (entity.isAggressive()) {
-					super.render(p_116352_, p_116353_, p_116354_, entity, p_116356_, p_116357_, p_116358_, p_116359_, p_116360_, p_116361_);
-				}
-			}
-		});
+		addLayer(new HideableItemInHandLayer<>(this, context.getItemInHandRenderer(), SHOULD_SHOW_ITEM_IN_HAND));
 		model.getHat().visible = true;
 	}
 
--- src/main/java/daripher/femalevillagers/compat/theconjurer/client/render/FemaleConjurerRenderer.java
+++ src/main/java/daripher/femalevillagers/compat/theconjurer/client/render/FemaleConjurerRenderer.java
@@ -1,31 +1,23 @@
 package daripher.femalevillagers.compat.theconjurer.client.render;
 
-import com.mojang.blaze3d.vertex.PoseStack;
+import java.util.function.Predicate;
 
 import daripher.femalevillagers.FemaleVillagersMod;
+import daripher.femalevillagers.client.render.layer.HideableItemInHandLayer;
 import daripher.femalevillagers.compat.theconjurer.client.model.FemaleConjurerModel;
 import daripher.femalevillagers.compat.theconjurer.entity.FemaleConjurer;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
 import net.minecraft.client.renderer.entity.IllagerRenderer;
-import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.resources.ResourceLocation;
 
 public class FemaleConjurerRenderer extends IllagerRenderer<FemaleConjurer> {
 	private static final ResourceLocation SKIN_LOCATION = new ResourceLocation(FemaleVillagersMod.MOD_ID, "textures/entity/female_conjurer.png");
+	private static final Predicate<FemaleConjurer> SHOULD_SHOW_ITEM_IN_HAND = e -> e.isCastingSpell();
 
 	public FemaleConjurerRenderer(Context context) {
 		super(context, new FemaleConjurerModel(context.bakeLayer(FemaleConjurerModel.LAYER_LOCATION)), 0.5F);
 //		addLayer(new ConjurerSunglassesLayer(this, new FemaleConjurerModel(context.bakeLayer(IllagerRenderRefs.CONJURER_GLASSES))));
-		addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()) {
-			@Override
-			public void render(PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, FemaleConjurer entitylivingbaseIn, float limbSwing,
-					float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-				if (entitylivingbaseIn.isCastingSpell())
-					super.render(matrixStackIn, bufferIn, packedLightIn, entitylivingbaseIn, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw,
-							headPitch);
-			}
-		});
+		addLayer(new HideableItemInHandLayer<>(this, context.getItemInHandRenderer(), SHOULD_SHOW_ITEM_IN_HAND));
 		model.getHat().visible = true;
 	}
 
